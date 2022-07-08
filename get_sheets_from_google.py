597f50c4e5fa011c05badd9bf473368b737accb4
BUTAJI91 <98452517+BUTAJI91@users.noreply.github.com>
2022-07-08 20:54:39 +0500

    Update get_sheets_from_google.py

--- get_sheets_from_google.py
+++ get_sheets_from_google.py
@@ -132,22 +132,22 @@ for elem in data:
             list_i.append(i)  # Пишем индекс строки (нужно для удаления строк БД, удаленных из Google)
 
             if elem[1] != DB_elem[2]:  # Если стоимость заказа была изменена
-                '''update_cost = f"""
+                update_cost = f"""
                     UPDATE test
                     SET cost_in_dollars = %s and cost_in_rubles = %s
                     WHERE id = {DB_elem[0]}
                 """
-                execute_query(connection, update_cost, [elem[1], elem[2]])'''
-                print("Стоимость заказа изменена: ", elem)
+                execute_query(connection, update_cost, [elem[1], elem[2]])
+                #print("Стоимость заказа изменена: ", elem)
 
             if elem[2] != DB_elem[4]:  # Если срок поставки был изменен
-                '''update_delivery_time = f"""
+                update_delivery_time = f"""
                     UPDATE test
                     SET delivery_time = %s
                     WHERE id = {DB_elem[0]}
                 """
-                execute_query(connection, update_delivery_time, elem[3])'''
-                print("Срок поставки изменен: ", elem)
+                execute_query(connection, update_delivery_time, elem[3])
+                #print("Срок поставки изменен: ", elem)
                 # Мог написать отправку в ТГ, но пришлось пропустить из-за времени
 
     if not find:
@@ -156,16 +156,19 @@ for elem in data:
             Половину дня пытался сделать одним запросом, что-то пошло не так.
         '''
         #print("Новая строка: ", elem)
-        #values = ", ".join(["%s"] * len(elem))
-        #insert_test_table = f"INSERT INTO test (order_number, cost_in_dollars, cost_in_rubles, delivery_time) VALUES ({values})"
-        #execute_query(connection, insert_test_table, elem)
-print(data)
-print(DB_data)
+        values = ", ".join(["%s"] * len(elem))
+        insert_test_table = f"INSERT INTO test (order_number, cost_in_dollars, cost_in_rubles, delivery_time) VALUES ({values})"
+        execute_query(connection, insert_test_table, elem)
+
+#print(data)
+#print(DB_data)
+
 id_delete_str = []
+
 #   Удаляем строки БД, которых уже нет в таблице Google
 for i, elem in enumerate(DB_data):
     if i not in list_i:
         id_delete_str.append(elem[0])
         #print("Строка для удаления: ", elem)
 
-#execute_query(connection, "DELETE FROM test WHERE id = %s", id_delete_str)
+execute_query(connection, "DELETE FROM test WHERE id = %s", id_delete_str)
